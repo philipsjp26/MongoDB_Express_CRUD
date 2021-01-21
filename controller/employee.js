72b4a2fc544adcfcb9bdcb523f8be733fb1c5cda
philipsjp26 <philipsjp12@gmail.com>
2021-01-21 18:33:43 +0700

    create delete & find one

--- controller/employee.js
+++ controller/employee.js
@@ -1,62 +1,103 @@
 const employee_models = require('../models/database');
 
 const add_data =  async(req,res) => {
-    try {
-        const employee_data = new employee_models({
-            name: req.body.name,
-            age: req.body.age
-        });
-        // check if employee is exist
-        const findEmployee = await employee_models.findOne({name: req.body.name}).exec();
+	try {
+		const employee_data = new employee_models({
+			name: req.body.name,
+			age: req.body.age
+		});
+		// check if employee is exist
+		const findEmployee = await employee_models.findOne({name: req.body.name}).exec();
 
-        if(findEmployee){
-            console.log("employee is exist")
-            return res.status(200).json({
-                message:"employee already exist"
-            })
-        }
+		if(findEmployee){
+			console.log("employee is exist")
+			return res.status(200).json({
+				message:"employee already exist"
+			})
+		}
 
-        const add_employee = await employee_data.save();
-        if(!add_employee){
-            console.log("cannot add data to database")
-            return null
-        }
+		const add_employee = await employee_data.save();
+		if(!add_employee){
+			console.log("cannot add data to database")
+			return null
+		}
 
-        return res.status(201).json({
-            status: "Success",
-            content: add_employee
-        })
-    } catch (error) {
-        console.log(error.message);
-        return res.status(500).json({
-            status: "error",
-            message: error.message
-        })
-    }
+		return res.status(201).json({
+			status: "Success",
+			content: add_employee
+		})
+	} catch (error) {
+		console.log(error.message);
+		return res.status(500).json({
+			status: "error",
+			message: error.message
+		})
+	}
 }
 
 const get_all_data = async(req,res) => {
-    try {
-        const get_all = await employee_models.find().sort({ name: -1 });
+	try {
+		const get_all = await employee_models.find().sort({ name: -1 });
 
-        if(get_all. length == 0){
-            console.log("Data not found")
-            return null
-        }
+		if(get_all.length == 0){
+			console.log("Data not found")
+			return null
+		}
 
-        return res.status(200).json({
-            status:"Success",
-            content: get_all
-        })
-    } catch (error) {
-        return res.status(500).json({
-            status: "failed",
-            content: error.message
-        })
-    }
+		return res.status(200).json({
+			status:"Success",
+			content: get_all
+		})
+	} catch (error) {
+		return res.status(500).json({
+			status: "failed",
+			content: error.message
+		})
+	}
+}
+const delete_by_id = async(req,res) => {
+	try {
+		const delete_data = await employee_models.findByIdAndDelete(req.params.id);
+		if(!delete_data){
+			console.log(`cannot delete data id:${req.params.id}`);
+			return null
+		}
+		return res.status(201).json({
+			status:'success deleted'
+		})
+	} catch (error) {
+		console.log(error.message);
+		return res.status(500).json({
+			status:'error',
+			message: error.message
+		})
+	}
+}
+const find_by_id = async(req,res) => {
+	try {
+		const get_data = await employee_models.findOne(
+			{ id: req.params.id }
+		).exec();
+		if(get_data.length == 0){
+			console.log("Data not found")
+			return null
+		}
+		return res.status(200).json({
+			status: "Success",
+			content: get_data
+		})
+	} catch (error) {
+		console.log(error.message);
+		return res.status(500).json({
+			status:"error",
+			message: error.message
+		});
+	}
 }
 
 module.exports = {
-    add_data,
-    get_all_data
+	add_data,
+	get_all_data,
+	find_by_id,
+	delete_by_id
 }
--- routes/employee.js
+++ routes/employee.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const employeeController = require('../controller');
 
-
 router.post('/add', employeeController.employee.add_data);
 router.get('/all', employeeController.employee.get_all_data);
-
+router.get('/id/:id', employeeController.employee.find_by_id);
+router.delete('/:id', employeeController.employee.delete_by_id);
 module.exports = router;
